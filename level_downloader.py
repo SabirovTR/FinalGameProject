72870cb062ba43540175f3f9ba23b3572a347333
SabirovTR <71315309+SabirovTR@users.noreply.github.com>
2020-12-18 00:47:05 +0300

    New more universal level downloader
    work with ne typo of space objects

--- level_downloader.py
+++ level_downloader.py
@@ -1,50 +1,82 @@
-def read_object_parameters(parameters_line, obj):
+from space_objects import *
+
+
+def read_level_parameters(parameters_line, level):
 
     parameters_list = parameters_line.split()
 
-    obj.type = parameters_list[1]
-    obj.static = bool(parameters_list[2])
-    obj.firm = bool(parameters_list[3])
-    obj.m = float(parameters_list[4])
-    obj.x = float(parameters_list[5])
-    obj.y = float(parameters_list[6])
-    obj.vx = float(parameters_list[7])
-    obj.vy = float(parameters_list[8])
-    obj.ax = float(parameters_list[9])
-    obj.ay = float(parameters_list[10])
+    level.episode = int(parameters_list[1])
+    level.number = int(parameters_list[2])
+    level.name = parameters_list[3]
+    level.dynamic = bool(parameters_list[4])
+    level.reflected = bool(parameters_list[5])
 
 
 def read_ship_parameters(parameters_line, ship):
 
     parameters_list = parameters_line.split()
 
-    ship.m = parameters_list[1]
-    ship.x = parameters_list[2]
-    ship.y = parameters_list[3]
-    ship.vx = parameters_list[4]
-    ship.vy = parameters_list[5]
-    ship.ax = parameters_list[6]
-    ship.ay = parameters_list[7]
+    ship.m = float(parameters_list[1])
+    ship.x = float(parameters_list[2])
+    ship.y = float(parameters_list[3])
+    ship.vx = float(parameters_list[4])
+    ship.vy = float(parameters_list[5])
+    ship.ax = float(parameters_list[6])
+    ship.ay = float(parameters_list[7])
+
+
+def read_obj_parameters(parameters_line, obj):
+
+    parameters_list = parameters_line.split()
+
+    obj.type = parameters_list[1]
+    obj.dynamic = bool(parameters_list[2])
+    obj.destroying = bool(parameters_list[3])
+    obj.x = float(parameters_list[4])
+    obj.y = float(parameters_list[5])
+
+    if obj.type == 'Planet':
+        obj.m = float(parameters_list[6])
+        obj.vx = float(parameters_list[7])
+        obj.vy = float(parameters_list[8])
+        obj.ax = float(parameters_list[9])
+        obj.ay = float(parameters_list[10])
+        obj.image = int(parameters_list[11])
+        print(obj.vy)
+
+    elif obj.type == 'AntiPlanet':
+        obj.m = float(parameters_list[6])
+        obj.vx = float(parameters_list[7])
+        obj.vy = float(parameters_list[8])
+        obj.ax = float(parameters_list[9])
+        obj.ay = float(parameters_list[10])
+        obj.image = int(parameters_list[11])
 
 
 def download_level(level_file):
 
     space_obj = []
-    with open(level_file) as input_file:
-        for line in input_file:
-            if len(line.strip()) == 0 or line[0] == '#':
+
+    with open(level_file) as level_description:
+        for line in level_description:
+            if len(line.strip()) == 0:
                 continue
-            object_class = line.split()[0]
-            if object_class == "SpaceObject":
-                obj = SpaceObject()
-                read_object_parameters(parameters_line, obj)
-                space_obj.append(obj)
-            elif object_type == "SpaceShip":
+            line_description = line.split()[0]
+            if line_description == 'Level':
+                level = Level()
+                read_level_parameters(line, level)
+
+            elif line_description == 'SpaceShip':
                 ship = SpaceShip()
-                read_ship_parameters(parameters_line, ship)
+                read_ship_parameters(line, ship)
 
-    return (space_obj, ship)
+            elif line_description == "SpaceObj":
+                obj = SpaceObj()
+                read_obj_parameters(line, obj)
+                space_obj.append(obj)
 
+    return(level, ship, space_obj)
 
+#download_level('Levels/1.txt')
 if __name__ == "__main__":
-    print("This module is not for direct call!")
+   print("This module is not for direct call!")
